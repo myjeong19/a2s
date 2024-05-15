9e1b26e1ad9393304fa50dd0a849bb31d1c8b3bf
Eunbizz <eb001020@naver.com>
2024-05-15 14:44:36 +0900

    Style. index

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Main from "./pages/main/main.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Main />
+    {/* <App /> */}
+  </React.StrictMode>
+);
---
+++ client/src/pages/main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./main.module.css";
+
+const main = () => {
+  return (
+    <div className={classes.home}>
+      <h1>A2S</h1>
+    </div>
+  );
+};
+
+export default main;
