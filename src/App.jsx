4e4c51ff42519c949c5548da84335666ef84303e
myjeong19 <myjeong19@naver.com>
2025-03-30 17:21:24 +0900

    fix. render issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import classes from "./css/App.module.css";
+import classes from './css/App.module.css';
 
 function App() {
   return (
@@ -30,7 +30,7 @@ function App() {
                 <h3>Contact</h3>
                 <nav>
                   <a className={classes.mono__p} href="">
-                    Github,{" "}
+                    Github,{' '}
                   </a>
                   <a className={classes.mono__p} href="">
                     Linkdin
@@ -51,41 +51,29 @@ function App() {
 
           <div className={classes.section2__information}>
             <div className={classes.section2__container}>
-              <div
-                className={`${classes.container__box} ${classes.roboto_mono}`}
-              >
+              <div className={`${classes.container__box} ${classes.roboto_mono}`}>
                 <h3>Education</h3>
-                <h3 className={classes.mono__p}>
-                  2019 ~ 2023 인천대학교 소비자학과
-                </h3>
+                <h3 className={classes.mono__p}>2019 ~ 2023 인천대학교 소비자학과</h3>
               </div>
               <div className={classes.container__box}>
                 <h3>Community</h3>
-                <h3 className={classes.mono__p}>
-                  2019 ~ 2023 인천대학교 소비자학과
-                </h3>
+                <h3 className={classes.mono__p}>2019 ~ 2023 인천대학교 소비자학과</h3>
               </div>
 
               <div className={classes.container__box}>
                 <h3>Experience</h3>
-                <h3 className={classes.mono__p}>
-                  2019 ~ 2023 인천대학교 소비자학과
-                </h3>
+                <h3 className={classes.mono__p}>2019 ~ 2023 인천대학교 소비자학과</h3>
               </div>
             </div>
 
             <div className={classes.section2__container}>
               <div className={classes.container__box}>
                 <h3>Skills</h3>
-                <h3 className={classes.mono__p}>
-                  2019 ~ 2023 인천대학교 소비자학과
-                </h3>
+                <h3 className={classes.mono__p}>2019 ~ 2023 인천대학교 소비자학과</h3>
               </div>
               <div className={classes.container__box}>
                 <h3>Award</h3>
-                <h3 className={classes.mono__p}>
-                  2019 ~ 2023 인천대학교 소비자학과
-                </h3>
+                <h3 className={classes.mono__p}>2019 ~ 2023 인천대학교 소비자학과</h3>
               </div>
             </div>
           </div>
--- src/pages/project/AnimatedTale.jsx
+++ src/pages/project/AnimatedTale.jsx
@@ -1,14 +1,14 @@
-import classes from "./css/ProjectDetail.module.css";
-import Gnb from "../layouts/Gnb";
-import demo_part from "../../assets/animatedTale/demo_part.mp4";
-import A2 from "../../assets/animatedTale/A2.png";
-import A3 from "../../assets/animatedTale/A3.png";
-import A4 from "../../assets/animatedTale/A4.png";
-import A5 from "../../assets/animatedTale/A5.png";
-import A6 from "../../assets/animatedTale/A6.jpg";
-import A7 from "../../assets/animatedTale/A7.png";
-import A8 from "../../assets/animatedTale/A8.jpg";
-import A9 from "../../assets/animatedTale/A9.png";
+import classes from './css/ProjectDetail.module.css';
+import Gnb from '../layouts/Gnb';
+import demo_part from '../../assets/animatedTale/demo_part.mp4';
+import A2 from '../../assets/animatedTale/A2.png';
+import A3 from '../../assets/animatedTale/A3.png';
+import A4 from '../../assets/animatedTale/A4.png';
+import A5 from '../../assets/animatedTale/A5.png';
+import A6 from '../../assets/animatedTale/A6.jpg';
+import A7 from '../../assets/animatedTale/A7.png';
+import A8 from '../../assets/animatedTale/A8.jpg';
+import A9 from '../../assets/animatedTale/A9.png';
 
 const AnimatedTale = () => {
   return (
@@ -48,9 +48,7 @@ const AnimatedTale = () => {
           <div className={classes.section2__container}>
             <div className={classes.container__box}>
               <h3>Supported by</h3>
-              <h3 className={classes.mono__p}>
-                Korea Art Management Service, ARTS KOREA LAB
-              </h3>
+              <h3 className={classes.mono__p}>Korea Art Management Service, ARTS KOREA LAB</h3>
             </div>
             <div className={classes.container__box}>
               <h3>Date</h3>
@@ -67,20 +65,19 @@ const AnimatedTale = () => {
         <div className={classes.contant__description}>
           <div className={classes.descriptionWrapper}>
             <h3>
-              본 프로젝트는 애니메이션 제작 과정에서의 복잡하고 시간 소모적인
-              작업들을 AI를 이용해 간소화 함으로써 일반 사용자들도 간단한
-              애니메이션 제작을 경험해볼 수 있도록 돕기 위해 제작되었습니다.
+              본 프로젝트는 애니메이션 제작 과정에서의 복잡하고 시간 소모적인 작업들을 AI를 이용해
+              간소화 함으로써 일반 사용자들도 간단한 애니메이션 제작을 경험해볼 수 있도록 돕기 위해
+              제작되었습니다.
             </h3>
             <h3>
-              본 프로젝트를 통해 사용자는 원하는 2D 캐릭터 그림에 모션을
-              적용하고 자동으로 렌더링 하여 gif 파일로 추출할 수 있습니다.
-              사용자는 원하는 모션을 선택하거나 직접 영상을 촬영하여 생성할 수
-              있습니다. 또한, 사용자의 선호에 따라, 캐릭터의 크기, 위치, 해상도,
+              본 프로젝트를 통해 사용자는 원하는 2D 캐릭터 그림에 모션을 적용하고 자동으로 렌더링
+              하여 gif 파일로 추출할 수 있습니다. 사용자는 원하는 모션을 선택하거나 직접 영상을
+              촬영하여 생성할 수 있습니다. 또한, 사용자의 선호에 따라, 캐릭터의 크기, 위치, 해상도,
               모션 속도 등을 조정할 수 있습니다.
             </h3>
             <h3>
-              본 프로젝트를 통해 애니메이션 제작의 접근성을 높여 누구나 쉽게
-              원하는 간단한 애니메이션을 제작할 수 있을 것으로 기대합니다.
+              본 프로젝트를 통해 애니메이션 제작의 접근성을 높여 누구나 쉽게 원하는 간단한
+              애니메이션을 제작할 수 있을 것으로 기대합니다.
             </h3>
           </div>
           <div className={classes.gap__small} />
@@ -102,9 +99,7 @@ const AnimatedTale = () => {
 
           <div className={classes.sub}>
             <h1>3D to 2D Retargetting</h1>
-            <h3>
-              3D bvh 모션 데이터를 2D 캐릭터의 뼈대에 적용하여 애니메이션 생성
-            </h3>
+            <h3>3D bvh 모션 데이터를 2D 캐릭터의 뼈대에 적용하여 애니메이션 생성</h3>
           </div>
           <img src={A4} className={classes.img__large} />
           <div className={classes.gap__small} />
